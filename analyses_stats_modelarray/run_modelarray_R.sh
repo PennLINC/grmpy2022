c85518b7f160d862296466b3baf5fe35bb8fbb11
Kahini Mehta <kahini_mehta@brown.edu>
2022-01-27 11:06:56 -0500

    add analyses for ConVoxel and ModelArray for asl cbf data

---
+++ analyses_stats_modelarray/run_modelarray_R.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# this is to call .R (or .Rmd) file of running ModelArray
+
+# +++++++++++++++++++++++++ VARIABLES TO CHANGE: ++++++++++++++++++++++++++++++++++++++++++++
+folder_main="/cbica/projects/GRMPY/project/curation/testing/stats_ModelArray"
+
+num_voxels=10   # 0 if requesting all voxels
+num_subj=215
+num_cores=4    # number of CPU cores; the more the faster
+
+fn_rstudio_singularity="/cbica/projects/GRMPY/software/rstudio_4.1.sif"
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+printf -v date '%(%Y%m%d-%H%M%S)T' -1   # $date, in YYYYmmdd-HHMMSS
+echo "date variable: ${date}"
+
+filename_output_body="GRMPY_meanCBF_n${num_subj}_nvoxels-${num_voxels}_wResults_${date}"
+fn_R_output="${folder_main}/${filename_output_body}.txt"
+
+#cmd="Rscript ./test_modelarray.R ${num_voxels} ${num_subj} ${num_cores} ${folder_main} ${filename_output_body} > ${fn_R_output} 2>&1 &"
+cmd="singularity run --cleanenv ${fn_rstudio_singularity} Rscript ./test_modelarray.R ${num_voxels} ${num_subj} ${num_cores} ${folder_main} ${filename_output_body} > ${fn_R_output} 2>&1 &"
+echo $cmd
+# copy the $cmd here manually (otherwise there might be issues with output $fn_R_output):
+singularity run --cleanenv ${fn_rstudio_singularity} Rscript ./test_modelarray.R ${num_voxels} ${num_subj} ${num_cores} ${folder_main} ${filename_output_body} > ${fn_R_output} 2>&1 &
---
+++ analyses_stats_modelarray/test_modelarray.R
@@ -0,0 +1,98 @@
+# if Rmd: search to see how to set up args and command to call .Rmd!
+
+# rm(list=ls())
+
+list.of.packages <- c("devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(devtools)
+# install ModelArray:
+branch_name_modelarray <- "enh/voxels"    # +++++++++++++++ TODO: change this after merging the branch into main!!!
+devtools::install_github("PennLINC/ModelArray", ref = branch_name_modelarray,
+                         upgrade = "never") # not to upgrade package dependencies
+library(ModelArray)
+
+### inputs #####
+args = commandArgs(trailingOnly=TRUE)
+
+num.voxels <- as.integer(args[1])  # if ==0, set as full set
+nsubj <- as.integer(args[2])   # number of subjects, e.g. 215
+num.cores <- as.integer(args[3])   # number of CPUs to use
+folder.main <- as.character(args[4]) # "/cbica/projects/GRMPY/project/curation/testing/stats_ModelArray"
+filename_output_body <- as.character(args[5])  # output filename (without extension), e.g. paste0("GRMPY_meanCBF_n",toString(nsubj))
+
+message(paste0("number of voxels to analyze = ", toString(num.voxels)))
+message(paste0("number of subjects = ", toString(nsubj)))
+message(paste0("number of cores = ", toString(num.cores)))
+message(paste0("main folder = ", folder.main))
+message(paste0("output .h5 filename = ", filename_output_body, ".h5"))
+
+# +++++++++++++++++++ CHANGE THESE INPUTS IF NEEDED: ++++++++++++++++++++++++++++++++++
+formula <- CBF ~ age + sex
+full.outputs <- TRUE
+analysis_name <- "lm_fullOutputs"    # the name for the statistical results dataframe to be saved into .h5 file
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+fn.h5.orig <- file.path(folder.main, 
+                        paste0("GRMPY_meanCBF_n",toString(nsubj),"_orig.h5"))
+# fn.h5.output <- file.path(folder.main, 
+#                         paste0("GRMPY_meanCBF_n",toString(nsubj),".h5"))
+fn.h5.output <- file.path(folder.main,
+                          paste0(filename_output_body,".h5"))
+fn.csv <- file.path(folder.main,
+                    paste0("GRMPY_convoxel_meanCBF_n",toString(nsubj),".csv"))
+
+# sanity check:
+if (file.exists(fn.h5.orig) == FALSE) {
+  stop(paste0("input .h5 file does not exist: "), fn.h5.orig)
+}
+if (file.exists(fn.csv) == FALSE) {
+  stop(paste0("input .csv file does not exist: "), fn.csv)
+}
+
+# generate fn.output:
+if (fn.h5.orig != fn.h5.output) {
+  file.copy(from=fn.h5.orig, to=fn.h5.output, overwrite = TRUE, copy.mode = TRUE, copy.date = TRUE)   # , recursive = TRUE
+}
+
+
+
+### set up for ModelArray #####
+phenotypes <- read.csv(fn.csv)
+#head(phenotypes)
+
+message("formula:")
+formula
+
+modelarray <- ModelArray(fn.h5.output, scalar_types = c("CBF"))
+
+
+# list of voxels to run:
+if (num.voxels == 0) {  # requesting all
+  element.subj <- NULL
+  num.voxels <- numElementsTotal(modelarray = modelarray, scalar_name="CBF")  # total number of voxels
+} else {
+  element.subset <- 1:num.voxels
+}
+
+
+### run ModelArray ######
+mylm <- ModelArray.lm(formula, data = modelarray, phenotypes = phenotypes, scalar = "CBF",
+                      element.subset = element.subset, full.outputs = full.outputs,
+                      correct.p.value.terms = "fdr", 
+                      correct.p.value.model = "fdr",
+                      pbar = TRUE, n_cores = num.cores)
+message("statistical results:")
+head(mylm)
+
+### save results #####
+writeResults(fn.h5.output, mylm, analysis_name = analysis_name, overwrite = TRUE)
+
+# if you want to view the saved results in .h5:
+rhdf5::h5ls(fn.h5.output)
+
+modelarray_new <- ModelArray(fn.h5.output, scalar_types = c("CBF"), analysis_names = analysis_name)
+message("after saving to .h5:")
+modelarray_new@results[[analysis_name]]
