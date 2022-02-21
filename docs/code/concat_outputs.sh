087cc574db653d4d1273d71e6a7e1510cfe7b8db
kahinimehta <68132139+kahinimehta@users.noreply.github.com>
2022-02-21 14:39:38 -0500

    updated code

--- docs/code/concat_outputs.sh
+++ docs/code/concat_outputs.sh
@@ -6,10 +6,10 @@ cd ${PROJECT_ROOT}
 cd  /cbica/projects/GRMPY/project/curation/testing
 datalad clone ria+file://${PROJECT_ROOT}/output_ria#~data concat_ds
 cd concat_ds/code
-wget https://raw.githubusercontent.com/PennLINC/grmpy2022/master/docs/code/concatenor.py
+wget https://raw.githubusercontent.com/PennLINC/grmpy2022/master/docs/code/concatenator.py
 cd ..
 datalad save -m "added concatenator script"
-datalad run -i 'sub-*rest*singleband*fsLR_desc-qc_bold.csv' -o '${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv' --expand inputs --explicit "python code/concatenator.py $PWD ${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv"
+datalad run -i 'sub-*rest*singleband*fsLR_desc-qc_bold.csv' -o '${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv' --expand inputs --explicit "python code/concatenator.py  ${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv"
 datalad save -m "generated report"
 # push changes
 datalad push
