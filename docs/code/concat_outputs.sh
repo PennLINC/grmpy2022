beae552eca044eb5ca12edac7f1ef5375331cf23
kahinimehta <68132139+kahinimehta@users.noreply.github.com>
2022-02-21 14:05:20 -0500

    updated files

---
+++ docs/code/concat_outputs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -u -x
+PROJECT_ROOT=/cbica/projects/GRMPY/project/curation/testing/xcp_outputs/XCP
+cd ${PROJECT_ROOT}
+# set up concat_ds and run concatenator on it
+cd  /cbica/projects/GRMPY/project/curation/testing
+datalad clone ria+file://${PROJECT_ROOT}/output_ria#~data concat_ds
+cd concat_ds/code
+wget https://raw.githubusercontent.com/PennLINC/grmpy2022/master/docs/code/concatenor.py
+cd ..
+datalad save -m "added concatenator script"
+datalad run -i 'sub-*rest*singleband*fsLR_desc-qc_bold.csv' -o '${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv' --expand inputs --explicit "python code/concatenator.py $PWD ${PROJECT_ROOT}/XCP_sub-*rest*singleband*fsLR_desc-qc_bold.csv_QC.csv"
+datalad save -m "generated report"
+# push changes
+datalad push
+# remove concat_ds
+git annex dead here
+cd ..
+chmod +w -R concat_ds
+rm -rf concat_ds
+echo SUCCESS
